a62de6343997f0c5ea8fa7dcc41a4dd2f210318a
ShishKebab99 <shishanth.kaliannan@gmail.com>
2018-08-07 23:06:32 +0800

    LaserBeam skill

---
+++ Assets/Scripts/LaserBowSkill.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBowSkill : MonoBehaviour {
+
+    public int attackDmg;
+    public GameObject damageCounter;
+    float rotate;
+    // Use this for initialization
+    void Start()
+    {
+        rotate = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<AI>().MinusHP(attackDmg);
+            var clone = (GameObject)Instantiate(damageCounter, other.transform.position, other.transform.rotation);
+            clone.GetComponentInChildren<DamageNumbers>().dmg = attackDmg;
+        }
+    }
+}
